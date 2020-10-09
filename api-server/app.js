5d1d139f32d5950dafa50f48068461c5bc2ec827
Miguel Sandoval <miguelship3@gmail.com>
2020-10-09 21:13:55 +0200

    Added package json .env file, gitignore and app.js intialized

---
+++ api-server/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+require('dotenv').config();
+
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const cors = require('cors');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const port = process.env.PORT;
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    console.log('Connected to DB');
+})
+.catch((err) => {
+    console.log('Not connected', err);
+});
+
+app.use(cors({
+    origin: ["http://localhost:3000", "localhost:3000"],
+    credentials: true
+}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.listen(port, () => {
+    console.log(`Listening at port: ${port}`);
+})
+
+module.exports = app;
